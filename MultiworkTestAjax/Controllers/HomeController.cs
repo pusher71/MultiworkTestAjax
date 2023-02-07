be8e44d240ea849c10709ba60841cb8b0d8c6679
Milus Kvadrigger <cool.b987@yandex.ru>
2023-02-07 05:32:01 +0300

    First commit

---
+++ MultiworkTestAjax/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+using MultiworkTestAjax.Models;
+
+namespace MultiworkTestAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Validate(string firstName, string lastName, string phoneNumber)
+        {
+            Thread.Sleep(10000);
+
+            string errorMessage = "<font color=\"red\">Ошибки содержат следующие поля:";
+            bool isAnyInvalid = false;
+            if (firstName == null)
+            {
+                errorMessage += " Имя";
+                isAnyInvalid = true;
+            }
+            if (lastName == null)
+            {
+                if (isAnyInvalid)
+                    errorMessage += ",";
+                errorMessage += " Фамилия";
+                isAnyInvalid = true;
+            }
+            if (phoneNumber == null || !IsPhoneNumberValid(phoneNumber))
+            {
+                if (isAnyInvalid)
+                    errorMessage += ",";
+                errorMessage += " Номер телефона";
+                isAnyInvalid = true;
+            }
+            errorMessage += "</font>";
+
+            return Json( new {
+                isValid = !isAnyInvalid,
+                message = isAnyInvalid
+                    ? errorMessage
+                    : "<font color=\"green\">Принято</font>"
+            });
+        }
+
+        private bool IsPhoneNumberValid(string phoneNumber)
+        {
+            Regex regex = new Regex(@"^[+0]\d+");
+            return regex.IsMatch(phoneNumber);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MultiworkTestAjax/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultiworkTestAjax.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
